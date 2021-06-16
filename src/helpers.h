15d7f6a03f48a1fc1732808dcf399c610fec4037
Albin Wahlén <alegow8@gmail.com>
2021-06-16 10:14:50 +0200

    Restore increment_datetime function

--- src/helpers.h
+++ src/helpers.h
@@ -38,6 +38,13 @@ void display_min_sec(void);
 
 void display_h_min(void);
 
+/* Increment a datetime struct. Set year, month and day to -1.
+ *
+ * datetime   Pointer to datetime struct.
+ * startIndex Where to start incrementing. 0 for incrementing a second, 1 for 
+ *              a minute etc. */
+void increment_datetime(datetime_t *t, int startIndex);
+
 void increment_with_wrap(int *num, int wrap);
 
 void decrement_with_wrap(int *num, int wrap);
