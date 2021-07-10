35f0f31f38476ea897b7e9cd174ddfbf73508aa3
Albin Wahlén <alegow8@gmail.com>
2021-07-10 12:21:10 +0200

    Setup orphan pin branch for testing and development

---
+++ src/pins.h
@@ -0,0 +1,25 @@
+#ifndef PINS_H_H
+#define PINS_H_H
+
+/**
+ * pins.h
+ *
+ * Define all the pin conections.
+ **/
+
+// TM1637 7-segment display
+#define CLK_PIN 27
+#define DIO_PIN 26
+
+ // Piezo buzzer
+#define BUZ_PIN 18 
+
+// Buttons
+#define RIGHT_BUTTON 22
+#define MIDDLE_BUTTON 21
+#define LEFT_BUTTON 20
+
+// LED
+#define LED_PIN 25
+
+#endif // PINS_H_h
